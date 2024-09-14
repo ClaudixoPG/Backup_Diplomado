3070ae57ea26be176284a89e1db425d504162640
Claudio Rubio <43611630+ClaudixoPG@users.noreply.github.com>
2024-09-14 09:59:55 -0300

    First Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    public float speed = 5.5f;
+    public float fireRate = 0.25f;
+    public int lives = 3;
+    public int shields = 3;
+    public float canFire = 0.0f; //Time to fire again
+    public float shieldDuration = 5.0f;
+
+    public GameObject BulletPref;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        CheckBoundaries();
+        ChangeWeapon();
+        //UseShields();
+        Fire();
+    }
+
+    //Character Movement, use WASD keys to move the player
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+    }
+
+    void CheckBoundaries()
+    {
+        //Check for boundaries of the game, use Main Camera to set the boundaries
+        var cam = Camera.main;
+        float xMax = cam.orthographicSize * cam.aspect;
+        float yMax = cam.orthographicSize;
+        if (transform.position.x > xMax)
+        {
+            transform.position = new Vector3(-xMax, transform.position.y, 0);
+        }
+        else if (transform.position.x < -xMax)
+        {
+            transform.position = new Vector3(xMax, transform.position.y, 0);
+        }
+        if (transform.position.y > yMax)
+        {
+            transform.position = new Vector3(transform.position.x, -yMax, 0);
+        }
+        else if (transform.position.y < -yMax)
+        {
+            transform.position = new Vector3(transform.position.x, yMax, 0);
+        }
+    }
+
+    //Player Fire
+    void Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > canFire)
+        {
+            Instantiate(BulletPref, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+            canFire = Time.time + fireRate;
+        }
+    }
+    //public GameObject BulletPref; ----> esta es la bala que se va a disparar
+
+    public List<Bullet> bullets;
+
+    public void ChangeWeapon()
+    {
+        //For changing weapons, use the number keys 1, 2, 3
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            BulletPref = bullets[0].gameObject;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            BulletPref = bullets[1].gameObject;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            BulletPref = bullets[2].gameObject;
+        }
+    }
+
+}
